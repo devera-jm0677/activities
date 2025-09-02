1af245df5566db8d8af27e19260aa2c27a3f1871
judymarie <147223511+devera-jm0677@users.noreply.github.com>
2025-09-02 20:07:34 +0800

    Update assignment1.py
    a1

--- assignment1.py
+++ assignment1.py
@@ -1,12 +1,21 @@
+print("ASSIGNMENT 1: SEARCH IN THE MATRIX")
+
 while True: 
     rw = int(input("Enter a number: "))
     cl = int(input("Enter a second number: "))
-    srch = int(input("Search a number: "))
+    
     
     if rw == 0 or cl == -1:
         print("Stop the loop")
         exit()
+
+    for q in range(1, rw + 1):
+        for w in range(1, cl + 1):
+            val = q*w
+            print(val, end="\t")
+        print()
     
+    srch = int(input("Search a number: "))
     for q in range(1, rw + 1):
         for w in range(1, cl + 1):
             val = q*w
@@ -15,7 +24,4 @@ while True:
             else:
                 print(val, end="\t")
         print()
-
-#how about add a matrix just for display before search function
-
-
+    print()
