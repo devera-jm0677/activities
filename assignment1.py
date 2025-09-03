fded37d950e562e9d75f4da5686f4e445f79c815
judymarie <147223511+devera-jm0677@users.noreply.github.com>
2025-09-03 21:09:42 +0800

    assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,22 +1,26 @@
-print("ASSIGNMENT 1: SEARCH IN THE MATRIX")
+print("ASSIGNMENT 1: MULTIPLICATION TABLE")
 
+# THE USER ENTERS A NUMBER OF ROWS AND COLUMNS
 while True: 
-    rw = int(input("Enter a number: "))
-    cl = int(input("Enter a second number: "))
+    rw = int(input("Enter a number (Enter 0 or -1 to terminate): "))
+    cl = int(input("Enter a second number (Enter 0 or -1 to terminate): "))
     
-    
+    # IF CONDITION FOR TERMINATION OF PROGRAM
     if rw == 0 or cl == -1:
         print("Stop the loop")
         exit()
 
+    # DISPLAYS THE TABLE
     for q in range(1, rw + 1):
         for w in range(1, cl + 1):
             val = q*w
             print(val, end="\t")
         print()
-    
+
+    # THE USER ENTERS A NUMBER TO BE SEARCHED
     srch = int(input("Search a number: "))
     for q in range(1, rw + 1):
+        # DISPLAYS THE TABLE, HIGHLIGHTING THE SEARCHED NUMBER
         for w in range(1, cl + 1):
             val = q*w
             if val == srch:
@@ -25,3 +29,4 @@ while True:
                 print(val, end="\t")
         print()
     print()
+
